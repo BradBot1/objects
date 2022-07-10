f7f4800b09a50ecadc5b4cbbdc8ced1752a75d58
BradBot_1 <45739045+BradBot1@users.noreply.github.com>
2022-07-10 14:44:47 +0100

    FEAT: better methods for PrimitiveMap

--- src/main/java/fun/bb1/objects/PrimitiveMap.java
+++ src/main/java/fun/bb1/objects/PrimitiveMap.java
@@ -2,8 +2,34 @@ package fun.bb1.objects;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.jetbrains.annotations.NotNull;
+
 public final class PrimitiveMap<K> extends ConcurrentHashMap<K, Primitive> {
 	
 	private static final long serialVersionUID = -2863274313069676442L;
+	
+	public final @NotNull Primitive put(@NotNull final K key, @NotNull final String value) {
+		return this.put(key, new Primitive(value));
+	}
+	
+	public final @NotNull Primitive put(@NotNull final K key, @NotNull final Character value) {
+		return this.put(key, new Primitive(value));
+	}
+	
+	public final @NotNull Primitive put(@NotNull final K key, @NotNull final Boolean value) {
+		return this.put(key, new Primitive(value));
+	}
+	
+	public final @NotNull Primitive put(@NotNull final K key, @NotNull final Number value) {
+		return this.put(key, new Primitive(value));
+	}
+	
+	public final @NotNull Primitive put(@NotNull final K key, @NotNull final Primitive[] value) {
+		return this.put(key, new Primitive(value));
+	}
+	
+	public final @NotNull Primitive put(@NotNull final K key, @NotNull final PrimitiveMap<?> value) {
+		return this.put(key, new Primitive(value));
+	}
 	
 }
