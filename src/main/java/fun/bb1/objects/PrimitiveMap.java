5543da784cf17ca5dabe8c273f434b95fc39912d
BradBot_1 <45739045+BradBot1@users.noreply.github.com>
2022-08-22 20:37:45 +0100

    CHORE: documentation

--- src/main/java/fun/bb1/objects/PrimitiveMap.java
+++ src/main/java/fun/bb1/objects/PrimitiveMap.java
@@ -1,9 +1,31 @@
 package fun.bb1.objects;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * 
+ * Copyright 2022 BradBot_1
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Used to contain a {@link Map} where a {@link Primitive} is always the type of a given value
+ * 
+ * @author BradBot_1
+ */
 public final class PrimitiveMap<K> extends ConcurrentHashMap<K, Primitive> {
 	
 	private static final long serialVersionUID = -2863274313069676442L;
