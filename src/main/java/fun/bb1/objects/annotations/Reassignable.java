05da3a47f2ec16655d9ea4773b12410baf80b26e
BradBot_1 <45739045+BradBot1@users.noreply.github.com>
2023-01-04 21:23:19 +0000

    FEAT: Reassignable annotation for fields + bump ver

---
+++ src/main/java/fun/bb1/objects/annotations/Reassignable.java
@@ -0,0 +1,34 @@
+package fun.bb1.objects.annotations;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * Copyright 2023 BradBot_1
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Denotes that a field has been left accessible and changeable on purpose and that re-assigning the value at the given field is expected behaviour
+ * 
+ * @author BradBot_1
+ */
+@Retention(RUNTIME)
+@Target(FIELD)
+public @interface Reassignable {
+	
+}
